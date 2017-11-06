0db788e6dce11db2586eea404335d282d1737206
Carlos Buruel <buruel.carlos@gmail.com>
2017-11-06 12:18:08 -0600

    fisrt commit

---
+++ WebApplication1/Service/IService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WebApplication1.Service
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoWork")]
+        String DoWork();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoSquare")]
+        int DoSquare(int value);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "DoAddValues")]
+        int DoAddValues(AddValues addValues);
+    }
+}
---
+++ WebApplication1/Service/Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.ServiceModel.Activation;
+
+namespace WebApplication1.Service
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Service1 : IService1
+    {
+        public int DoSquare(int value)
+        {
+            return value * value;
+        }
+
+        public String DoWork()
+        {
+            return "Hello REST WCF Service!";
+        }
+
+        public int DoAddValues(AddValues addValues)
+        {
+            return addValues.Value1 + addValues.Value2;
+        }
+    }
+
+    [DataContract]
+    public class AddValues
+    {
+        [DataMember]
+        public int Value1 { get; set; }
+        [DataMember]
+        public int Value2 { get; set; }
+
+        public AddValues()
+        {
+            Value1 = 0;
+            Value2 = 0;
+        }
+    }
+}
